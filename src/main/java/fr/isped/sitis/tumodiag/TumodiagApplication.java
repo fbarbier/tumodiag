291ab3a5ebbc184b972d0e43972901c9822b42ca
Fabien Barbier <fabien.barbier@gmail.com>
2020-01-21 10:39:07 +0100

    Initial Spring Boot Config
    Defined application.properties

---
+++ src/main/java/fr/isped/sitis/tumodiag/TumodiagApplication.java
@@ -0,0 +1,13 @@
+package fr.isped.sitis.tumodiag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TumodiagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TumodiagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/isped/sitis/tumodiag/TumodiagApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isped.sitis.tumodiag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TumodiagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
